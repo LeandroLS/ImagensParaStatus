53393eb5bd80c1b88f6ccb225b28897c2ebd332a
Leandro <llimas@outlook.com>
2019-02-16 14:29:53 -0200

    more tests. add remove db.

--- database/collectionImages.js
+++ database/collectionImages.js
@@ -1,27 +1,33 @@
 const MongoClient = require('mongodb').MongoClient;
-// Connection URL
 const url = 'mongodb://localhost:27017';
 const dbName = 'EscreverNaImagem';
 const collection = 'Images';
-const client = new MongoClient(url);
+const client = new MongoClient(url, { useNewUrlParser: true });
 
-function insert(image){
-    client.connect((err) => {
-        if(err) throw err;
-        let db = client.db(dbName);
-        let col = db.collection(collection);
-        col.insertOne(image, (err, result) => {
-            if(err) throw err;
-        });
-    });
-    client.close();
-}
-
-async function list(query = {}){
+async function connectDB(){
     try {
         await client.connect();
         let db = client.db(dbName);
         let col = db.collection(collection);
+        return col;
+    } catch (error) {
+        throw error
+    }
+
+}
+async function insert(image){
+    try {
+        let col = await connectDB();
+        let result = await col.insertOne(image);
+        return result;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function list(query = {}){
+    try {
+        let col = await connectDB();
         let result = await col.find(query).toArray();
         return result;
     } catch (err) {
@@ -29,7 +35,17 @@ async function list(query = {}){
     }
 }
 
+async function remove(imagem) {
+    try {
+        let col = await connectDB();
+        let result = await col.remove(imagem, { justOne:true } );
+        return result;
+    } catch (err) {
+        throw err;
+    }
+}
 module.exports = {
     insert,
-    list
+    list,
+    remove
 };
--- index.js
+++ index.js
@@ -5,36 +5,42 @@ const imageDB = require('./database/collectionImages');
 app.get('/', (req, res) => {
     let images = imageDB.list();
     images.then((images) => {
-        res.render('index', {
+        return res.render('index', {
             images : images
         });
     }).catch((err) => {
-        res.render('index');
+        return res.render('index');
     });
 });
 
 app.get('/upload-images', (req, res) => {
-    res.render('upload-images');
+    return res.render('upload-images');
 });
 
 app.get('/edit-image/:fileName', (req, res) => {
     let query = { 'fileName' : req.params.fileName };
     let images = imageDB.list(query);
     images.then((image) => {
-        res.render('edit-image', { image : image[0] });
+        return res.render('edit-image', { image : image[0] });
     }).catch((err) => {
-        res.render('edit-image');
+        return res.render('edit-image');
     });
 });
 
 app.post('/images', upload.single('image'), (req, res) => {
     if(req.file){
-        imageDB.insert({
+        let image = imageDB.insert({
+            enabled: true,
             originalName: req.file.originalname, 
             fileName: req.file.filename
         });
+        image.then((result)=>{
+            return res.redirect(301,'upload-images');
+        }).catch((err)=>{
+            return res.redirect(301,'upload-images');
+        });
     }
-    res.redirect(301,'upload-images');
+    return res.redirect(301,'upload-images');
 });
 
 app.listen(8000);
