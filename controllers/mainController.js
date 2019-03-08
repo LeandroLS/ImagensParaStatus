a56a20d4370a3c1f991a0296741533931710864d
Leandro <llimas@outlook.com>
2019-03-07 21:34:25 -0300

    fazendo paginação por frase funcionar

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -31,10 +31,14 @@ app.get('/privacidade', (req, res) => {
     res.render('privacidade');
 });
 app.get('/:category?', checkIfCategoryExists, async (req, res) => {
+    let { phrase } = req.query;
     let { category } = req.params;
     var filter = {};
     if(category){
         filter = { category : category };
+    }
+    if(phrase){
+        filter = { phrase: {$regex: `.*${phrase}.*`, $options:'i'}};
     }
     let header = getImagesCategoryHeader(category);
     let imagesPerPage = app.locals.imagesPerPage;
@@ -48,32 +52,33 @@ app.get('/:category?', checkIfCategoryExists, async (req, res) => {
             images : data[1],
             categories : data[2],
             categoryPagination : category,
-            header : header,
+            phrase : phrase,
+            header : header
         });
     });
 });
 
 app.get('/:category?/page/:number', (req, res) => {
     let imagesPerPage = app.locals.imagesPerPage;
+    let { phrase } = req.query;
+    var filter = {};
+    var filterNumberOfPages = {};
     if(req.params.number){
         var pageNumber = req.params.number;
-        var filterImages = {
-            id : { $gt : parseInt((pageNumber * imagesPerPage) - imagesPerPage) }
-        };
+        filter.id = { $gt : parseInt((pageNumber * imagesPerPage) - imagesPerPage) };
         if(req.params.category){
             var category = req.params.category;
             var header = getImagesCategoryHeader(category);
-            var filterNumberOfPages = { category : category };
-            filterImages.category = category;
-        } else {
-            var filterNumberOfPages = {};
+            filterNumberOfPages.category = category;
         }
-    } else {
-        var filterImages = {};
+        if(phrase){
+            filter.phrase = { $regex: `.*${phrase}.*`, $options:'i' };
+            filterNumberOfPages.phrase = { $regex: `.*${phrase}.*`, $options:'i' };
+        }
     }
     let db = app.locals.db;
     let numberOfPages = db.collection('Images').countDocuments(filterNumberOfPages).then(qtdImages => calcNumberOfPages(qtdImages));
-    let images = db.collection('Images').find(filterImages).limit(imagesPerPage).toArray().then(images => images);
+    let images = db.collection('Images').find(filter).limit(imagesPerPage).toArray().then(images => images);
     let categories = db.collection('Categories').find().toArray().then(categories => categories);
     Promise.all([images, categories, numberOfPages]).then(data => {
         return res.render('index', {
@@ -81,25 +86,8 @@ app.get('/:category?/page/:number', (req, res) => {
             categories : data[1],
             numberOfPages : data[2],
             categoryPagination : category,
-            header : header
-        });
-    });
-});
-
-app.get('/search/phrase', (req, res) => {
-    let { phrase } = req.query;
-    let db = app.locals.db;
-    let imagesPerPage = app.locals.imagesPerPage;
-    let header = 'Buscando por: ' + phrase;
-    let images = db.collection('Images').find({ phrase: {$regex: `.*${phrase}.*`, $options:'i'}}).limit(imagesPerPage).toArray().then(images => images);
-    let categories = db.collection('Categories').find().toArray().then(categories => categories);
-    let numberOfPages = db.collection('Images').countDocuments({ phrase: {$regex: `.*${phrase}.*`, $options:'i'}}).then(qtdImages => calcNumberOfPages(qtdImages));
-    Promise.all([images, categories, numberOfPages]).then(data => {
-        return res.render('index', {
-            images : data[0],
-            categories : data[1],
-            numberOfPages : data[2],
-            header : header
+            header : header,
+            phrase : phrase
         });
     });
 });
