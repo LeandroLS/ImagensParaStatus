d3126f5f418d277e5c3e49624097f79bf198ad15
Leandro <llimas@outlook.com>
2019-03-12 19:45:23 -0300

    Alterando nome de imagens upadas para conterem a categoria e parte da frase.

--- config/upload.js
+++ config/upload.js
@@ -6,7 +6,9 @@ const storage = multer.diskStorage({
         cb(null, 'public/images/original-images');
     },
     filename: function (req, file, cb) {
-        cb(null, `${file.fieldname}-${moment().format('DDMMYYYY-hh-mm-ss')}.${mime.extension(file.mimetype)}`);
+        let { category, phrase, existentCategory } = req.body;
+        category = category || existentCategory;
+        cb(null, `${category}-${phrase.slice(0,25)}.${mime.extension(file.mimetype)}`);
     }
 });
 
