17d71bf61b7be1bce5fd3ace682201e3abaef949
Leandro <llimas@outlook.com>
2019-03-13 22:49:58 -0300

    .

--- config/express.js
+++ config/express.js
@@ -10,8 +10,9 @@ app.set('views', path.viewsPath);
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/../public/'));
 function wwwRedirect(req, res, next) {
-    if (req.headers.host.slice(0, 4) === 'www.' && req.protocol === 'http' || req.protocol === 'https') {
-        var newHost = req.headers.host.slice(4);
+    let host = req.headers.host;
+    if (host.slice(0, 4) === 'www.' && req.protocol === 'http' || req.protocol === 'https') {
+        let newHost = host.slice(4);
         return res.redirect(301, 'https://' + newHost + req.originalUrl);
     } else if (req.protocol === 'http') {
         return res.redirect(301, 'https://' + req.headers.host + req.originalUrl);
