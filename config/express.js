fb12d75d8abd97e6917a2e411e2fd0c8e1454e4b
Leandro <llimas@outlook.com>
2019-03-13 22:20:17 -0300

    .

--- config/express.js
+++ config/express.js
@@ -10,7 +10,11 @@ app.set('views', path.viewsPath);
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/../public/'));
 function wwwRedirect(req, res, next) {
-    return res.redirect(301, req.protocol + '://' + 'imagensparastatus.com.br' + req.originalUrl);
+    if (req.headers.host.slice(0, 4) === 'www.') {
+        var newHost = req.headers.host.slice(4);
+        return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
+    }
+    next();
 };
 
 app.set('trust proxy', true);
