795c555606b261d13940885c23085f017976c6d5
Leandro <llimas@outlook.com>
2019-03-13 23:18:00 -0300

    mudando ambiente

--- config/express.js
+++ config/express.js
@@ -10,17 +10,19 @@ app.set('views', path.viewsPath);
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/../public/'));
 function wwwRedirect(req, res, next) {
-    let host = req.headers.host;
-    if (host.slice(0, 4) === 'www.' && (req.protocol === 'http' || req.protocol === 'https')) {
-        let newHost = host.slice(4);
-        return res.redirect(301, 'https://' + newHost + req.originalUrl);
-    } else if (req.protocol === 'http') {
-        return res.redirect(301, 'https://' + req.headers.host + req.originalUrl);
+    if(process.env.AMBIENTE == 'production'){
+        let host = req.headers.host;
+        if (host.slice(0, 4) === 'www.' && (req.protocol === 'http' || req.protocol === 'https')) {
+            let newHost = host.slice(4);
+            return res.redirect(301, 'https://' + newHost + req.originalUrl);
+        } else if (req.protocol === 'http') {
+            return res.redirect(301, 'https://' + req.headers.host + req.originalUrl);
+        }
     }
     next();
 };
 
 app.set('trust proxy', true);
 app.use(wwwRedirect);
-app.use(compression());
+app.use(compression({threshold : 0}));
 module.exports = app;
